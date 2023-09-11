9edaf46da7cfa2f94fb85dae949ef3a43e668b44
kaungkhantzawjudgify <kaung@judgify.me>
2023-09-11 09:04:31 +0630

    change webhook

--- webhook.php
+++ webhook.php
@@ -1,38 +1,22 @@
 <?php
-  require 'vendor/autoload.php';
-  require_once 'constants/SampleCodeConstants.php';
 
-  $headers = getallheaders();
-  $payload = file_get_contents("php://input");
-  $webhook = new AuthnetWebhook(\SampleCodeConstants::AUTHNET_SIGNATURE, $payload, $headers);
-  if ($webhook->isValid()) {
+$input = file_get_contents('php://input');
+echo json_encode($input);
 
-      $data = json_encode($webhook);
+// if ($_SERVER['REQUEST_METHOD'] !== 'POST' || json_last_error() !== JSON_ERROR_NONE)
+// {
+//     http_response_code(400);
 
-      $myfile = fopen(time()."webhook.txt", "w") or die("Unable to open file!");
-      fwrite($myfile, $data);
-      fclose($myfile);
-      // Get the transaction ID
-      $transactionId = $webhook->payload->id;
-  
-      // Here you can get more information about the transaction
-      $request  = AuthnetApiFactory::getJsonApiHandler(\SampleCodeConstants::AUTHNET_LOGIN, AUTHNET_TRANSKEY);
-      $response = $request->getTransactionDetailsRequest(array(
-          'transId' => $transactionId
-      ));
-  
-      /* You can put these response values in the database or whatever your business logic dictates.
-      $response->transaction->transactionType
-      $response->transaction->transactionStatus
-      $response->transaction->authCode
-      $response->transaction->AVSResponse
-      */
-}
-else 
-{
-  $data = "ERROR";
+//     echo json_encode([ 'error' => 'Invalid request.' ]);
+//     die();
+// }
 
-  $myfile = fopen(time()."error.txt", "w") or die("Unable to open file!");
-  fwrite($myfile, $data);
-  fclose($myfile);
-}
+$decode = $input ? json_encode($input) :( $_GET ? json_encode($_GET): json_encode($_POST)) ;
+
+$myfile = fopen(time()."webhook.log", "w") or die("Unable to open file!");
+
+fwrite($myfile, $decode);
+
+fclose($myfile);
+
+return;
