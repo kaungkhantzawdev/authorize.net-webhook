b8cdf6930d0011b2673e6e1aeaefda473869e5d1
kaungkhantzawjudgify <kaung@judgify.me>
2023-09-13 18:54:15 +0630

    modify webhook php

--- webhook.php
+++ webhook.php
@@ -1,7 +1,22 @@
 <?php
 
 $input = file_get_contents('php://input');
-echo json_encode($input);
+echo "respone print";
+
+echo "I'm from post";
+print_r($_POST);
+echo "<br>";
+
+echo "I'm from get";
+print_r($_GET);
+echo "<br>";
+
+echo "I'm FROM Input";
+print_r($input);
+echo "<br>";
+
+echo "I'm from server";
+print_r($_SERVER);
 
 // if ($_SERVER['REQUEST_METHOD'] !== 'POST' || json_last_error() !== JSON_ERROR_NONE)
 // {
@@ -13,9 +28,17 @@ echo json_encode($input);
 
 $decode = $input ? json_encode($input) :( $_GET ? json_encode($_GET): json_encode($_POST)) ;
 
-$myfile = fopen(time()."webhook.log", "w") or die("Unable to open file!");
+$myfile = fopen(time()."webhook_server.log", "w") or die("Unable to open file!");
 
-fwrite($myfile, $decode);
+fwrite($myfile, json_encode($_SERVER));
+
+$myfile_get = fopen(time()."webhook_get.log", "w") or die("Unable to open file!");
+
+fwrite($myfile_get, json_encode($_GET));
+
+$myfile_post = fopen(time()."webhook_post.log", "w") or die("Unable to open file!");
+
+fwrite($myfile_post, json_encode($_POST));
 
 fclose($myfile);
 
