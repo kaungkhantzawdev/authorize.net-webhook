c63e3233e8189a60bd7475e97049748838192f04
kaungkhantzawjudgify <kaung@judgify.me>
2023-09-15 21:20:13 +0630

    remove key

--- src/sha512.php
+++ src/sha512.php
@@ -3,7 +3,7 @@
 function generateSH512($textToHash)
 {
 
-    $signatureKey = "6D3545F065A58EAB059D04207B5D91F0D4B1100D9E8FF0563D212B214BABBAFBADD48162811AA81F5B7DA1BF5C73BD45A9ADD6F984C68F26A1E1A02453F75EB8";
+    $signatureKey = "sig key";
     
     if ($textToHash != null) 
     {
