d5aa7f049bd9a5807edbbe3ea99e0b91f026dbc9
kaungkhantzawjudgify <kaung@judgify.me>
2023-09-16 19:07:37 +0630

    webhook edit

--- webhook.php
+++ webhook.php
@@ -1,24 +1,9 @@
+
 <?php
 
 require_once 'src/sha512.php';
 
 $input = file_get_contents('php://input');
-echo "respone print";
-
-echo "I'm from post";
-print_r($_POST);
-echo "<br>";
-
-echo "I'm from get";
-print_r($_GET);
-echo "<br>";
-
-echo "I'm FROM Input";
-print_r($input);
-echo "<br>";
-
-echo "I'm from server";
-print_r($_SERVER);
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST' || json_last_error() !== JSON_ERROR_NONE)
 {
@@ -28,24 +13,37 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST' || json_last_error() !== JSON_ERROR_NO
     die();
 }
 
-$input =  $input;
+
+
+
 $x_anet_sig = $_SERVER['HTTP_X_ANET_SIGNATURE'];
+$input_p = json_decode($input);
+$arr = array(
+"eventType" => $input_p->eventType,
+"responseCode" => $input_p->payload->responseCode,
+"authAmount" => $input_p->payload->authAmount,
+"merchantReferenceId" => $input_p->payload->merchantReferenceId,
+"invoiceNumber" => $input_p->payload->invoiceNumber,
+
+);
 
 if(generateSH512($input) === $x_anet_sig )
 {
-    $_input = fopen(time()."webhook_input.log", "w") or die("Unable to open file!");
+    $_input = fopen(time()."webhook_success_input.log", "w") or die("Unable to open file!");
 
-    fwrite($_input, $input);
+    fwrite($_input, json_encode($arr));
     
-    $myfile = fopen(time()."webhook_server.log", "w") or die("Unable to open file!");
+    $myfile = fopen(time()."webhook_success_server.log", "w") or die("Unable to open file!");
     
-    fwrite($myfile, $x_anet_sig);
-       
-}
+    fwrite($myfile, json_encode(array("input" => $input, "x_anet_sig" => $x_anet_sig)));
+
+}else {
 
 $_error = fopen(time()."webhook_error.log", "w") or die("Unable to open file!");
 
 fwrite("Webhook Error, not same with signature", $_error);
 
+}
 
 return;
+
